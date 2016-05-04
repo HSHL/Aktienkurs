2e4beb9ae61cc250968ca4bc685c22a0c9928cbc
Gary733 <jramermann@gmail.com>
2016-05-04 13:18:03 +0200

    tt

--- FetchData.cpp
+++ FetchData.cpp
@@ -5,3 +5,24 @@
  */
 
 #include "FetchData.h"
+
+FetchData::FetchData(){
+
+};
+
+void FetchData::GetPage(QString url){
+    
+    // content
+    QByteArray content("a=root&b=root&c=submit");
+ 
+    // header
+    QHttpRequestHeader header("POST", "/nyphel/Essais_stage/essai.php");
+    header.setValue("Host", "www.membres.lycos.fr");
+    header.setContentType("application/x-www-form-urlencoded"); // important
+    header.setContentLength(content.length());
+    
+    // request
+    QHttp http("www.membres.lycos.fr");
+    http.request(header, content);
+    
+};
